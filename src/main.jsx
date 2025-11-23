541f1ca9f46228c6348c0e0d18d9d5f68c9968b8
frfadhilah1995 <frfadhilah@gmail.com>
2025-11-24 00:31:37 +0700

    Add basename for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import { AuthProvider } from './context/AuthContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/kasir">
       <AuthProvider>
         <DatabaseProvider>
           <App />
